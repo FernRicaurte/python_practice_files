1e89eb59625654b67433770ad8a7ea54b6fcbaab
FernRicaurte <FernRicaurte2@gmail.com>
2023-09-30 20:35:11 -0400

    adding pseudocode to robot_inheritance file

--- robot_inheritance.py
+++ robot_inheritance.py
@@ -8,7 +8,7 @@ class Robot:
         self.position[0] = self.position[0] + x
         print("New position:", self.position)
         
-class Robot_Dog(Robot):
+class Robot_Dog(Robot): # The Robot_Dog class will inherit the name, position properties and print out the passed in name, since it did not have its own __init__ method; it will also inherit the walk method that establishes a new position the passed x value. It will, however, have its own make_noise method which prints "Woof woof!"
         def make_noise(self):
             print("Woof Woof!")
             
