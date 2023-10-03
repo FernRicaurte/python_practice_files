2cf217e1a8e7c8d9e03a69d2e101158de299f24d
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-02 21:28:34 -0400

    finalized the SalaryEmployee class with super(), added pseudocode

--- employee.py
+++ employee.py
@@ -4,11 +4,10 @@ class Employee:
         self.lname = lname
         
 
-class Salary Employee:
+class SalaryEmployee(Employee): # I want the new SalaryEmployee to derive from parent Employee class, so I pass it in.
     def __init__(self, fname, lname, salary):
-        self.fname = fname
-        self.lname = lname
-        self.salary = salary
+        super().__init__(fname, lname) # Within the __init__ method, I can call the parent Employee class __init__ method with super().__init__ and then pass in lname and fname in parentheses.
+        self.salary = salary # This property/value is exclusive to the SalaryEmployee class.
         
     def calculate_paycheck(self):
         return self.salary/52
