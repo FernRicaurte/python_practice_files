b2f6526e67a8cf3dd25030910c36ac34e6ea1005
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-02 21:20:09 -0400

    renamed Employee to SalaryEmployee, created new Employee parent class

--- employee.py
+++ employee.py
@@ -1,4 +1,10 @@
 class Employee:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+        
+
+class Salary Employee:
     def __init__(self, fname, lname, salary):
         self.fname = fname
         self.lname = lname
