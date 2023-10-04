0ce69a5057ce023a0dffc1c7eecb7ef8fa00f523
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-03 21:31:53 -0400

    adding CommissionEmployee child class

--- employee.py
+++ employee.py
@@ -21,3 +21,16 @@ class HourlyEmployee(Employee):
         
     def calculate_paycheck(self):
         return self.weekly_hours * self.hourly_rate
+    
+
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, fname, lname, salary, sales_num, com_rate):
+        super().__init__(fname, lname, salary)
+        self.sales_num = sales_num
+        self.com_rate = com_rate
+        
+    def calculate_paycheck(self):
+        regular_salary = super().calculate_paycheck()
+        total_commission = self.sales_num * self.commission_rate
+        return regular_salary + total_commission
+    
