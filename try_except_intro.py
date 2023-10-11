722f0222302f8095d794a7a4fd3b630849e12f95
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-10 22:52:45 -0400

    adding new line to file

--- try_except_intro.py
+++ try_except_intro.py
@@ -5,3 +5,5 @@ try:
     definition = acronyms = acronyms["BTW"] # <--- Code that might cause an exception
 except:
     print("Key does not exist") # <--- Print an error message. When the program runs, the error message will print and now the program can keep running.
+print("The program keeps going...")
+    
