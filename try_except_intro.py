270d3b5824659667ec4bf581e03cf9a767d10387
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-11 14:44:53 -0400

    deleted a repetition

--- try_except_intro.py
+++ try_except_intro.py
@@ -2,7 +2,7 @@ acronyms = {"LOL": "laugh out loud",
             "IDK": "I don't know",
             "TBH": "to be honest"}
 try:
-    definition = acronyms = acronyms["BTW"] # <--- Code that might cause an exception
+    definition = acronyms["BTW"] # <--- Code that might cause an exception
 except:
     print("Key does not exist") # <--- Print an error message. When the program runs, the error message will print and now the program can keep running.
 print("The program keeps going...")
