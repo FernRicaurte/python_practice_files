d81ae45a316b7913f32f1331a5baa26f7ab842ca
FernRicaurte <FernRicaurte2@gmail.com>
2023-10-22 13:19:04 -0400

    adding print of definition for correct input

--- try_except_intro.py
+++ try_except_intro.py
@@ -3,6 +3,11 @@ acronyms = {"LOL": "laugh out loud",
             "TBH": "to be honest"}
 try:
     definition = acronyms["BTW"] # <--- Code that might cause an exception
+    print(definition)
 except:
-    print("Key does not exist") # <--- Print an error message. When the program runs, the error message will print and now the program can keep running.
+    print("The key does not exist") # <--- Print an error message. When the program runs, the error message will print and now the program can keep running.
+finally: 
+    print("The acronyms we have defined are:")
+    for acronym in acronyms:
+        print(acronym)
 print("The program keeps going...")
